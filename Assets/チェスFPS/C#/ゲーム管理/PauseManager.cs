c3e7d2366677080debaea599bd63f27957ba6cf6
HOMUHOMU\himao <himaomoteamasu1@gmail.com>
2025-06-05 13:12:21 +0900

    ポーズメニュを追加。ボタンが表示されないが、機能はしている

---
+++ Assets/チェスFPS/C#/ゲーム管理/PauseManager.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseManager : MonoBehaviour
+{
+    public GameObject UI_pauseMenu;
+    private bool isPaused = false;
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape)){
+        Debug.Log("Esc pressed");
+            if(isPaused) Resume();
+            else         Pause();
+        
+        }
+    }
+
+    public void Resume()
+    {
+        UI_pauseMenu.SetActive(true);
+        Time.timeScale = 1f;
+        isPaused = true;
+    }
+
+    public void Pause()
+    {
+        Debug.Log("Pause実行中");
+        Time.timeScale = 1f;
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    public void Restart()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void QuitGame()
+    {
+        Time.timeScale = 1f;
+        Application.Quit();
+    #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false; // エディタ用
+    #endif
+    }
+
+}
